351ec825b98301311474254a68b9970c7e41a090
Ali Khan <alik95997@gmail.com>
2025-11-01 17:52:06 +0500

    vercel changed

--- server.js
+++ server.js
@@ -14,16 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ CORS configuration
 app.use(
-  cors({
-    origin: [
-      "https://expense-tracker-frontend-psi-dusky.vercel.app",
-      "http://localhost:5173",
-      "http://localhost:5000"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-  })
+  cors()
 );
 
 // ✅ Middleware
