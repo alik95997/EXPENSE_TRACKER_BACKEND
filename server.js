352ebe206afed0fea210795c8dfac00f3b5abffe
Ali Khan <alik95997@gmail.com>
2025-11-01 14:58:44 +0500

    Initial commit

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Import dotenv at the very top
 import dotenv from 'dotenv';
 dotenv.config();
-
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/expenseRoutes.js";
@@ -13,7 +12,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Simple CORS - accepts requests from anywhere
-app.use(cors());
+app.use(cors({
+  origin: '*'  // Sab origins allow
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
