5c532e7f9c90bfae2639c67af5b01db8332a46df
Ali Khan <alik95997@gmail.com>
2025-11-01 23:06:21 +0500

    added verfyToken in income Routes

--- routes/incomeRoutes.js
+++ routes/incomeRoutes.js
@@ -4,7 +4,7 @@ import { verifyToken } from "../middlewear/authMiddlewear.js";
 
 const router = express.Router();
 
-router.post("/addincome", verifyToken addIncome)
+router.post("/addincome", verifyToken ,addIncome)
 router.get("/getincome", verifyToken, getIncome)
 // FIX: Added the URL parameter :id to match the frontend request format
 router.delete("/deleteincome/:id", verifyToken, deleteIncome)
