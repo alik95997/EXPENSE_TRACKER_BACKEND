fc10f26a0e8703594fe13c0e4beed55a5d8200ab
Ali Khan <alik95997@gmail.com>
2025-12-16 13:36:38 +0500

    h

--- controller/authController.js
+++ controller/authController.js
@@ -38,7 +38,7 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // true only in production
+      secure: false, // true only in production
       sameSite: "none", // needed for cross-origin
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
