c1bbb6eef57c7192bc535d43a670142f2e4b8c8a
Ali Khan <alik95997@gmail.com>
2025-12-16 12:54:29 +0500

    changes in auth controller

--- controller/authController.js
+++ controller/authController.js
@@ -37,8 +37,7 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true, // JS access nahi kar sakta
-      secure: true, // HTTPS only (production)
-      sameSite: "none", // frontend & backend different domains
+
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
