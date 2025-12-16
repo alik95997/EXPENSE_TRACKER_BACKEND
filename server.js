a6881977aa3f698b4ebfc572c4bcc4174d1415f2
Ali Khan <alik95997@gmail.com>
2025-12-16 12:51:19 +0500

    cors policy changed

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ import cookieParser from "cookie-parser";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 app.use(cookieParser());
 
 app.use(express.json());
