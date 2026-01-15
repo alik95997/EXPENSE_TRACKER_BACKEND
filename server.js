bd73199bb99deeaa95105eb50e7f42d21f469912
Ali Khan <alik95997@gmail.com>
2026-01-15 22:21:34 +0500

    expense-tracker-frontend-psi-dusky.vercel.app

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://expense-tracker-frontend-i6gudd8xf-alik95997s-projects.vercel.app" 
+    "https://expense-tracker-frontend-psi-dusky.vercel.app" 
   ],
   credentials: true
 }));
